a0471cd904a597f027c1e7073351d65ed6b6f7dc
studioKeywi <github@studiokeywi.dev>
2020-09-25 09:31:47 -0500

    Correct relative SRCs again

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export { default as buildKonami } from '//cdn.jsdelivr.net/gh/studiokeywi/fun@latest/konami/index.js';
-export { default as buildConfetti } from '//cdn.jsdelivr.net/gh/studiokeywi/fun@latest/confetti/index.js';
+export { default as buildKonami } from '//cdn.jsdelivr.net/gh/studiokeywi/play/konami/index.js';
+export { default as buildConfetti } from '//cdn.jsdelivr.net/gh/studiokeywi/play/confetti/index.js';
