788aef9b5f872613a8039a8d38d974d760efeb5c
studioKeywi <github@studiokeywi.dev>
2020-09-24 20:58:49 -0500

    Update src's to CDN

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export { default as buildKonami } from './konami/index.js';
-export { default as buildConfetti } from './confetti/index.js';
+export { default as buildKonami } from '//cdn.jsdelivr.net/gh/studiokeywi/fun@latest/konami/index.js';
+export { default as buildConfetti } from '//cdn.jsdelivr.net/gh/studiokeywi/fun@latest/confetti/index.js';
